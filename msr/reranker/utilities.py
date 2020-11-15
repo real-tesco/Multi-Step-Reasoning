b25bd95fc30e757e1600c150fdc7044801b88de7
Tesco <lucaforprez@web.de>
2020-11-15 12:40:48 +0100

    get_mrr added to utilities.py

--- msr/reranker/utilities.py
+++ msr/reranker/utilities.py
@@ -94,6 +94,34 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
+def get_mrr(qrels: str, trec: str, metric: str = 'mrr_cut_10') -> float:
+    k = int(metric.split('_')[-1])
+    qrel = {}
+    with open(qrels, 'r') as f_qrel:
+        for line in f_qrel:
+            qid, _, did, label = line.strip().split()
+            if qid not in qrel:
+                qrel[qid] = {}
+            qrel[qid][did] = int(label)
+    run = {}
+    with open(trec, 'r') as f_run:
+        for line in f_run:
+            qid, _, did, _, _, _ = line.strip().split()
+            if qid not in run:
+                run[qid] = []
+            run[qid].append(did)
+    mrr = 0.0
+    for qid in run:
+        rr = 0.0
+        for i, did in enumerate(run[qid][:k]):
+            if qid in qrel and did in qrel[qid] and qrel[qid][did] > 0:
+                rr = 1 / (i + 1)
+                break
+        mrr += rr
+    mrr /= len(run)
+    return mrr
+
+
 def evaluate_run_with_trec_eval(qrels, prediction, path_to_trec_eval):
     logger.info(f"path to trec eval file: {path_to_trec_eval}")
     cmd = os.path.join(path_to_trec_eval, f" trec_eval {qrels} {prediction} -m map -m recip_rank -m ndcg")
