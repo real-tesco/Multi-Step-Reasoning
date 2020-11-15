009b9a573f8f450b81f85901d82689afacd207e5
Tesco <lucaforprez@web.de>
2020-11-15 13:07:44 +0100

    changed loss function to margin loss changed eval function wip TODO: refactor dataset, and data loaders

--- msr/reranker/train_ranker.py
+++ msr/reranker/train_ranker.py
@@ -109,7 +109,7 @@ def load_qrels(path_to_qrels):
 
 
 # TODO: look here
-def train_binary_classification(args, ranking_model, optimizer, train_loader):
+def train_binary_classification(args, loss, ranking_model, optimizer, device, train_loader, dev_loader):
     para_loss = utils.AverageMeter()
     ranking_model.train()
 
@@ -117,17 +117,16 @@ def train_binary_classification(args, ranking_model, optimizer, train_loader):
         if ex is None:
             continue
 
-        inputs = [e if e is None or type(e) != type(ex[0]) else Variable(e.cuda())
+        inputs = [e if e is None or type(e) != type(ex[0]) else Variable(e.to(device))
                   for e in ex[:3]]
         ranker_input = [*inputs[:]]
 
         scores_positive, scores_negative = ranking_model.score_documents(*ranker_input)  # todo: look here
-        true_labels_positive = torch.ones_like(scores_positive)
-        true_labels_negative = torch.zeros_like(scores_negative)
+        #true_labels_positive = torch.ones_like(scores_positive).to(device)
+        #true_labels_negative = torch.zeros_like(scores_negative).to(device)
 
-        loss = torch.nn.BCEWithLogitsLoss()
-        batch_loss = loss(scores_positive, true_labels_positive)
-        batch_loss += loss(scores_negative, true_labels_negative)
+        batch_loss = loss(scores_positive, scores_negative, torch.ones(scores_positive.size()).to(device))
+        #batch_loss += loss(scores_negative, true_labels_negative).item()
 
         optimizer.zero_grad()
         batch_loss.backward()
@@ -147,29 +146,32 @@ def train_binary_classification(args, ranking_model, optimizer, train_loader):
             para_loss.reset()
 
 
-def eval_ranker(args):
-    logger.info("Load Model")
-
+def eval_ranker(args, model, dev_loader, device):
     logger.info("Evaluating trec metrics for dev set...")
+    rst_dict = {}
+    for step, dev_batch in enumerate(dev_loader):
+        # TODO: msmarco dataset refactoring
+        query_id, doc_id, label, retrieval_score = dev_batch['query_id'], dev_batch['doc_id'], dev_batch['label'], \
+                                                   dev_batch['retrieval_score']
+        with torch.no_grad():
 
-    with open(args.dev_queries, "rb") as f:
-        dev_queries = torch.from_numpy(np.load(f)).cuda()
-    with open(args.dev_qids, "rb") as f:
-        dev_qids = np.load(f)
+            batch_score, _, _ = model.score_documents(dev_batch['q_embedding'].to(device),
+                                                      dev_batch['d_embedding'].to(device))
 
-    with open(args.out_file, "w") as f:
-        for idx, qid in enumerate(dev_qids):
-            ranked_docids = []
-            current_rank = 1
-            for idy, (label, distance) in enumerate(zip(labels[idx], distances[idx])):
-                docid = args.pid2docid_dict[str(label)]
-                if docid in ranked_docids:
-                    continue
-                f.write("{} Q0 {} {} {} {}\n".format(qid, docid, current_rank, 1.0 - distance, args.hnsw_index))
-                current_rank += 1
-                ranked_docids.append(docid)
+            # TODO: write to file
+            batch_score = batch_score.detach().cpu().tolist()
+            for (q_id, d_id, b_s, l) in zip(query_id, doc_id, batch_score, label):
+                if q_id in rst_dict:
+                    rst_dict[q_id].append((b_s, d_id, l))
+                else:
+                    rst_dict[q_id] = [(b_s, d_id, l)]
+        if (step + 1) % (4 * args.print_every) == 0:
+            print(f"-- eval: {step + 1}/{len(dev_loader)} --")
+    utils.save_trec(args.out_file, rst_dict)
+
     logger.info("Done with evaluation, use trec_eval to evaluate run...")
-    # _, _, _ = utils.evaluate_run_with_trec_eval(args.qrels_file, args.out_file, args.trec_eval)
+    mrr = utils.get_mrr(args.qrels_dev_file, args.out_file)
+    return mrr
 
 
 def main(args):
@@ -177,10 +179,20 @@ def main(args):
     logger.info('Starting load data...')
     logger.info(f'using cuda: {args.cuda}')
     logger.info(f'args train: {args.train}')
+    # TODO: DEV LOADER
+    logger.info(f'loading dev data..')
+    with open(args.dev_queries, "rb") as f:
+        dev_queries = torch.from_numpy(np.load(f))
+    with open(args.dev_qids, "rb") as f:
+        dev_qids = np.load(f)
+    logger.info(f'creating dev loader...')
+    dev_loader = None
 
     with open(args.pid2docid, 'r') as f:
         pid2docid = json.load(f)
         args.pid2docid_dict = pid2docid
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # initialize Model
     if args.checkpoint:
@@ -189,10 +201,14 @@ def main(args):
     else:
         logger.info('Initializing model from scratch...')
         ranker_model, optimizer = init_from_scratch(args)
-    if args.cuda:
-        ranker_model.cuda()
+
+    ranker_model.to(device)
+    loss = torch.nn.MarginRankingLoss(margin=1)
+    loss = loss.to(device)
+
     if args.train:
         logger.info("Starting training...")
+        best_mrr = 0.0
         for epoch in range(0, args.epochs):
             stats['epoch'] = epoch
             # need to load the training data in chunks since its too big
@@ -207,13 +223,15 @@ def main(args):
                     triples = np.load(os.path.join(args.training_folder, "train.triples_msmarco.npy"))
                     triple_ids = None
                 training_loader = make_dataloader(pid2docid, triples, triple_ids, train_time=True)
-                train_binary_classification(args, ranker_model, optimizer, training_loader)
+                train_binary_classification(args, loss, ranker_model, optimizer, device, training_loader, dev_loader)
 
-            logger.info('checkpointing  model at {}.ckpt'.format(args.model_name))
-            save(args, ranker_model, optimizer, args.model_name + ".ckpt", epoch=stats['epoch'])
-        save(args, ranker_model, optimizer, args.model_name + ".max", epoch=stats['epoch'])
-    if args.eval:
-        eval_ranker(args)
+            if (epoch+1) % args.eval_every == 0:
+                rst = eval_ranker(args, ranker_model, dev_loader, device)
+                if rst > best_mrr:
+                    best_mrr = rst
+                    logger.info('New best MRR = {:2.4f}'.format(rst))
+                    logger.info('checkpointing  model at {}.ckpt'.format(args.model_name))
+                    save(args, ranker_model, optimizer, args.model_name + ".ckpt", epoch=stats['epoch'])
 
 
 if __name__ == '__main__':
