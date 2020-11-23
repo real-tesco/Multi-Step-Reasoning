e7b9bad648e562ef1fb205b947b3f99a4b1277eb
Tesco <lucaforprez@web.de>
2020-11-23 11:00:23 +0100

    test

--- msr/data/datasets/rankingdataset.py
+++ msr/data/datasets/rankingdataset.py
@@ -22,9 +22,8 @@ class RankingDataset(Dataset):
         self._query_ids = []
 
         # Load documents and convert to tensors
-        self._doc_ids.append(np.load(x) for x in doc_ids_files)
+        self._doc_ids = [np.load(x) for x in doc_ids_files]
         print(len(self._doc_ids))
-        print(len(self._doc_ids[0]))
         self._docs.extend(torch.tensor(np.load(x)) for x in doc_embedding_files)
         self._docs = torch.cat(self._docs, dim=0)
         print(self._docs.shape)
