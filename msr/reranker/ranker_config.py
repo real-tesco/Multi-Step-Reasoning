e7030d8b4f1372d2a2b2ef651f64925ef2709bb7
Tesco <lucaforprez@web.de>
2020-11-28 13:26:49 +0100

    missing arg

--- msr/reranker/ranker_config.py
+++ msr/reranker/ranker_config.py
@@ -15,7 +15,7 @@ def get_args():
     parser.register('type', 'bool', str2bool)
 
     # ranker model options
-    parser.add_argument('-extra_layer', type=int, default=0, help='dim of extra layer if 0 no layer will be added')
+    parser.add_argument('-extra_layer', type=int, default=2500, help='dim of extra layer if 0 no layer will be added')
     parser.add_argument('-ranker_input', type=int, default=768,
                         help="dimension of the input to the ranker, should be twice of the embedding dim")
     parser.add_argument('-ranker_hidden', type=int, default=768,
