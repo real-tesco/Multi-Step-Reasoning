cf2b8ef104f65263984dc2b3d1cd3cdeb5e11cf8
real-tesco <lucaforprez@web.de>
2020-12-18 14:27:28 +0100

    sorted scores for avg replacement

--- train_reformulator.py
+++ train_reformulator.py
@@ -72,14 +72,14 @@ def eval_pipeline(args, knn_index, ranking_model, reformulator, dev_loader, devi
             #batch_score = batch_score.detach().cpu().tolist()
 
             # sort doc embeddings according score and reformulate
-            _, scores_sorted_indices = torch.sort(batch_score, dim=1, descending=True)
+            sorted_scores, scores_sorted_indices = torch.sort(batch_score, dim=1, descending=True).to(device)
             sorted_docs = document_embeddings[
                 torch.arange(document_embeddings.shape[0]).unsqueeze(-1), scores_sorted_indices].to(device)
 
             if args.reformulation_type == 'neural':
                 new_queries = reformulator(query_embeddings, sorted_docs)
             elif args.reformulation_type == 'weighted_avg':
-                new_queries = reformulator(sorted_docs, batch_score)
+                new_queries = reformulator(sorted_docs, sorted_scores)
             elif args.reformulation_type == 'transformer':
                 new_queries = reformulator(query_embeddings, sorted_docs)
             else:
