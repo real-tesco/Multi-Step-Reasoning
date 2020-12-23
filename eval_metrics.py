f5131c8f8b54a006fb9747638a8a4cd529317fa3
real-tesco <lucaforprez@web.de>
2020-12-23 12:23:21 +0100

    eval metrics updated

--- eval_metrics.py
+++ eval_metrics.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import sys
 import pytrec_eval
+import msr.metrics.metric
 
 
 def main():
@@ -16,37 +17,9 @@ def main():
     assert os.path.exists(args.qrel)
     assert os.path.exists(args.run)
 
-    with open(args.qrel, 'r') as f_qrel:
-        qrel = pytrec_eval.parse_qrel(f_qrel)
+    metrics = msr.metrics.metric.Metric()
 
-    with open(args.run, 'r') as f_run:
-        run = pytrec_eval.parse_run(f_run)
-
-    evaluator = pytrec_eval.RelevanceEvaluator(
-        qrel, pytrec_eval.supported_measures)
-
-    results = evaluator.evaluate(run)
-
-    def print_line(measure, scope, value):
-        print('{:25s}{:8s}{:.4f}'.format(measure, scope, value))
-
-    for query_id, query_measures in sorted(results.items()):
-        for measure, value in sorted(query_measures.items()):
-            print_line(measure, query_id, value)
-
-    # Scope hack: use query_measures of last item in previous loop to
-    # figure out all unique measure names.
-    #
-    # TODO(cvangysel): add member to RelevanceEvaluator
-    #                  with a list of measure names.
-    for measure in sorted(query_measures.keys()):
-        print_line(
-            measure,
-            'all',
-            pytrec_eval.compute_aggregated_measure(
-                measure,
-                [query_measures[measure]
-                 for query_measures in results.values()]))
+    _ = metrics.eval_run(args.qrel, args.run)
 
 
 if __name__ == "__main__":
