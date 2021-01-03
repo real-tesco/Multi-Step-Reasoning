d10e1f621c0a4acc94c037d6503301a72b423b37
Tesco <lucaforprez@web.de>
2021-01-03 13:27:33 +0100

    metric now counts number of unjudged documents in total and avg per query

--- msr/metrics/metric.py
+++ msr/metrics/metric.py
@@ -38,7 +38,7 @@ class Metric():
                 if qid not in run:
                     run[qid] = []
                 run[qid].append(did)
-        
+
         mrr = 0.0
         for qid in run:
             rr = 0.0
@@ -79,7 +79,7 @@ class Metric():
         mrr /= len(run)
         return mrr, query_mrr_dict
 
-    def eval_run(self, qrels: str, trec: str) -> Dict[str, float]:
+    def eval_run(self, qrels: str, trec: str, print_graphs=False) -> Dict[str, float]:
         query_measure_to_check = ['ndcg', 'map', 'P_10', 'recall_5', 'recall_10', 'recall_30', 'recall_100']
         with open(qrels, 'r') as f_qrel:
             qrel = pytrec_eval.parse_qrel(f_qrel)
@@ -99,59 +99,74 @@ class Metric():
         mrr_values = [mrr_dict[qid] for qid in mrr_dict]
         std_dev = np.std(mrr_values)
 
+        # calc number of unjudged documents returned
+        cnt = 0
+        total = 0
+        for qid in run:
+            for rst in run[qid]:
+                for docid in rst:
+                    if docid not in qrel[qid]:
+                        cnt += 1
+                        total += 1
+                    else:
+                        total += 1
+
         mes['mrr'] = mrr
         for key in mes:
             print("{}: {:.4f}".format(key, mes[key]))
         print(f"\nstd_dev of mrr: {std_dev:2.4f}\n")
+        print(f"Unjudged documents in result: {cnt}/{total}")
+        print(f"Unjudged documents avg per query: {cnt / len(run)}/{total / len(run)}")
 
-        query_measure_to_print = ['recall_100', 'recall_30', 'ndcg', 'map', 'P_10', 'mrr_cut_100']
+        if print_graphs:
+            query_measure_to_print = ['recall_100', 'recall_30', 'ndcg', 'map', 'P_10', 'mrr_cut_100']
 
-        # 6 ticks on axis
-        buckets = [[[] for _ in range(6)] for _ in range(len(query_measure_to_print))]
+            # 6 ticks on axis
+            buckets = [[[] for _ in range(6)] for _ in range(len(query_measure_to_print))]
 
-        for query_id, query_measures in sorted(results.items()):
-            # print(type(query_measures))
-            # for measure, value in sorted(query_measures.items()):
-            for i, measure in enumerate(query_measure_to_print):
-                if measure == 'mrr_cut_100':
-                    value = mrr_dict[query_id]
-                else:
-                    value = query_measures[measure]
-                if value <= 0.1:
-                    buckets[i][0].append(query_id)
-                elif value <= 0.3:
-                    buckets[i][1].append(query_id)
-                elif value <= 0.5:
-                    buckets[i][2].append(query_id)
-                elif value <= 0.7:
-                    buckets[i][3].append(query_id)
-                elif value <= 0.9:
-                    buckets[i][4].append(query_id)
-                else:
-                    buckets[i][5].append(query_id)
-        xs = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0]
-        ys = [[len(b[i]) / len(results) for i in range(len(b))] for b in buckets]
+            for query_id, query_measures in sorted(results.items()):
+                # print(type(query_measures))
+                # for measure, value in sorted(query_measures.items()):
+                for i, measure in enumerate(query_measure_to_print):
+                    if measure == 'mrr_cut_100':
+                        value = mrr_dict[query_id]
+                    else:
+                        value = query_measures[measure]
+                    if value <= 0.1:
+                        buckets[i][0].append(query_id)
+                    elif value <= 0.3:
+                        buckets[i][1].append(query_id)
+                    elif value <= 0.5:
+                        buckets[i][2].append(query_id)
+                    elif value <= 0.7:
+                        buckets[i][3].append(query_id)
+                    elif value <= 0.9:
+                        buckets[i][4].append(query_id)
+                    else:
+                        buckets[i][5].append(query_id)
+            xs = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0]
+            ys = [[len(b[i]) / len(results) for i in range(len(b))] for b in buckets]
 
-        fig = plt.figure(figsize=(10, 10))
+            fig = plt.figure(figsize=(10, 10))
 
-        sub1 = plt.subplot(2, 3, 1)
-        sub2 = plt.subplot(2, 3, 2)
-        sub3 = plt.subplot(2, 3, 3)
-        sub4 = plt.subplot(2, 3, 4)
-        sub5 = plt.subplot(2, 3, 5)
-        sub6 = plt.subplot(2, 3, 6)
+            sub1 = plt.subplot(2, 3, 1)
+            sub2 = plt.subplot(2, 3, 2)
+            sub3 = plt.subplot(2, 3, 3)
+            sub4 = plt.subplot(2, 3, 4)
+            sub5 = plt.subplot(2, 3, 5)
+            sub6 = plt.subplot(2, 3, 6)
 
-        plots = [sub1, sub2, sub3, sub4, sub5, sub6]
+            plots = [sub1, sub2, sub3, sub4, sub5, sub6]
 
-        for i, sub in enumerate(plots):
-            sub.plot(xs, ys[i], 'b.')
-            sub.set_title(query_measure_to_print[0])
-            sub.set_xticks(xs)
-            sub.set_xlabel('measure value bucket')
-            sub.set_ylabel('fraction in bucket')
+            for i, sub in enumerate(plots):
+                sub.plot(xs, ys[i], 'b.')
+                sub.set_title(query_measure_to_print[0])
+                sub.set_xticks(xs)
+                sub.set_xlabel('measure value bucket')
+                sub.set_ylabel('fraction in bucket')
 
-        fig.suptitle(trec, fontsize=14)
-        plt.savefig(trec + '.png')
+            fig.suptitle(trec, fontsize=14)
+            plt.savefig(trec + '.png')
 
         return mes
 
