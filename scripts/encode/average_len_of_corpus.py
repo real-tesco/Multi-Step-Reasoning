ffda3f3b92cc15427b5cf06ea35c3a1ac878092b
real-tesco <lucaforprez@web.de>
2021-01-13 15:54:13 +0100

    added script to check avg length of given corpus

--- scripts/encode/average_len_of_corpus.py
+++ scripts/encode/average_len_of_corpus.py
@@ -8,8 +8,9 @@ args = parser.parse_args()
 
 with open(args.corpus, "r") as input_f:
     avg_len = 0
-    number_of_docs = len(input_f)
+    number_of_docs = 0
     for line in tqdm(input_f):
         _, _, _, content = line.split('\t')
         avg_len += len(nltk.word_tokenize(content))
+        number_of_docs += 1
     print(f"Average length of given dataset: {avg_len/number_of_docs}")
