790898647a515ce5c46466c03a24d301112747d3
real-tesco <lucaforprez@web.de>
2021-01-14 14:39:44 +0100

    count avrg number also of generated jsonl file

--- scripts/encode/average_len_of_corpus.py
+++ scripts/encode/average_len_of_corpus.py
@@ -1,6 +1,7 @@
 import argparse
 from tqdm import tqdm
 import nltk
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('corpus')
@@ -10,7 +11,11 @@ with open(args.corpus, "r") as input_f:
     avg_len = 0
     number_of_docs = 0
     for line in tqdm(input_f):
-        _, _, _, content = line.split('\t')
-        avg_len += len(nltk.word_tokenize(content))
+        if args.corpus.split(".")[-1] == "tsv":
+            _, _, _, content = line.split('\t')
+        elif args.corpus.split(".")[-1] == "jsonl":
+            j = json.loads(line)
+            content = j["contents"]
+        avg_len += len(content.split(" "))
         number_of_docs += 1
     print(f"Average length of given dataset: {avg_len/number_of_docs}")
