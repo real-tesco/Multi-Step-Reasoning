c4187dfb923129bb8556bf09a81c58cbcc436748
real-tesco <lucaforprez@web.de>
2021-01-31 14:23:12 +0100

    sampling test prints, queries unsqueeze

--- inference.py
+++ inference.py
@@ -247,6 +247,7 @@ def test_clustering(args, knn_index, ranking_model, reformulator, test_loader, m
     timer.stop()
     for k, v in stats.items():
         print(f"{k}: {v}")
+    print(f"{stats['query_sil_score'] / stats['count']}")
     msr.utils.save_trec_inference(args.res + ".test", rst_dict_test)
 
     logger.info(f"Time needed per query: {timer.time() / (len(test_loader) * args.batch_size)} s")
