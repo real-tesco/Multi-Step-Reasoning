1d6d0f1cb6ae1704818cf5163cf7d598bf9bcfb1
real-tesco <lucaforprez@web.de>
2021-02-01 08:00:42 +0100

    new qs can now be avg of old and new qs, also able to not use ranker in second round

--- inference.py
+++ inference.py
@@ -223,16 +223,20 @@ def test_clustering(args, knn_index, ranking_model, reformulator, test_loader, m
 
         for step_s in range(args.number_samples):
             new_queries = sampled_docs[:, step_s]
+            if args.avg_new_qs:
+                new_queries = (query_embeddings + new_queries) / 2
             document_labels, document_embeddings, distances, _ = knn_index.knn_query_embedded(
                 new_queries.cpu(), k=args.retrieves_per_sample)
 
-            batch_score = ranking_model.rerank_documents(new_queries.to(device), document_embeddings.to(device), device)
-            #batch_score = ranking_model.rerank_documents(query_embeddings, document_embeddings.to(device), device)
-
-            # normalize batch score for comparability across different queries
-            for idy in range(0, batch_score.shape[0]):
-                batch_score[idy] = (batch_score[idy] - batch_score[idy].min()) / \
-                                   (batch_score[idy].max() - batch_score[idy].min())
+            if args.use_ranker_in_next_round:
+                batch_score = ranking_model.rerank_documents(new_queries.to(device), document_embeddings.to(device), device)
+                #batch_score = ranking_model.rerank_documents(query_embeddings, document_embeddings.to(device), device)
+                # normalize batch score for comparability across different queries
+                for idy in range(0, batch_score.shape[0]):
+                    batch_score[idy] = (batch_score[idy] - batch_score[idy].min()) / \
+                                       (batch_score[idy].max() - batch_score[idy].min())
+            else:
+                batch_score = torch.tensor(distances)
 
             batch_score = batch_score.detach().cpu().tolist()
 
