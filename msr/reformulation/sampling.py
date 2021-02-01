adcf1003fb13e9c5998b5edc399bf1cc32b777ad
real-tesco <lucaforprez@web.de>
2021-02-01 16:12:35 +0100

    input fix

--- msr/reformulation/sampling.py
+++ msr/reformulation/sampling.py
@@ -50,7 +50,7 @@ def cluster_sampling(documents, queries, qrels, document_labels, query_labels, n
                     stats['sil_score_cluster_min'] = sil_score_cluster
                 stats['sil_score_cluster'] += sil_score_cluster
                 if rel_docids is not None:
-                    retrieved_docs = document_labels[b][kmeans.labels_ == lbl]
+                    retrieved_docs = document_labels[b, kmeans.labels_ == lbl]
                     cnt_rel_in_cluster = [[] for _ in range(number_samples)]
                     for retrieved_doc_lbl in retrieved_docs:
                         cnt_rel_in_cluster[lbl].append(retrieved_doc_lbl)
