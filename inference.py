940c43ae1b3506666a95cecaa16aa65ff9841662
real-tesco <lucaforprez@web.de>
2021-02-04 11:57:06 +0100

    attention sampling wip adding weights print test

--- inference.py
+++ inference.py
@@ -762,12 +762,12 @@ def main():
                 for name, layer in reformulator.named_modules():
                     # print("name: ", name)
                     # print("layer: ", layer)
-                    if name == f'layers.{args.num_encoder_layers-1}.self_attn.out_proj':
-                        layer.register_forward_hook(save_attention_hook)
-                        print('added hook on layer: ', layer)
-                    #elif name == f"layers.{args.num_encoder_layers-1}.self_attn":
+                    #if name == f'layers.{args.num_encoder_layers-1}.self_attn.out_proj':
                     #    layer.register_forward_hook(save_attention_hook)
                     #    print('added hook on layer: ', layer)
+                    if name == f"layers.{args.num_encoder_layers-1}.self_attn":
+                        layer.register_forward_hook(save_attention_hook)
+                        print('added hook on layer: ', layer)
     else:
         reformulator = None
 
