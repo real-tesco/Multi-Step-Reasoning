6a549e297e327156dc8f6921f6dd9cdb1599be22
real-tesco <lucaforprez@web.de>
2021-02-05 16:05:39 +0100

    attention sampling with original bert model

--- msr/knn_retriever/two_tower_bert.py
+++ msr/knn_retriever/two_tower_bert.py
@@ -35,10 +35,10 @@ class TwoTowerBert(nn.Module):
 
     def get_attention_heads(self, q_input_ids, q_input_mask, q_segment_ids):
         output = self._query_model(q_input_ids, attention_mask=q_input_mask, token_type_ids=q_segment_ids,
-                                   output_attentions=True, use_cache=True)
-        print(len(output))
-        for i in range(len(output)):
-            print(f"{i}: {type(output[i])}")
+                                   use_cache=True, output_attentions=True, return_dict=True)
+
+        for k, v in output:
+            print(f"{k}: {type(v)}")
         attention = output[2]
         print(type(attention))
         for i in range(len(attention)):
