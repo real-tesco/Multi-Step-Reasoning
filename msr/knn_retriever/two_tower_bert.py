e46ba22c2eff198ab74d6f287d3eb66cf35ec084
real-tesco <lucaforprez@web.de>
2021-02-05 17:53:19 +0100

    attention sampling with original bert model

--- msr/knn_retriever/two_tower_bert.py
+++ msr/knn_retriever/two_tower_bert.py
@@ -48,7 +48,7 @@ class TwoTowerBert(nn.Module):
         keys = output['past_key_values']
         #print(f"shape keys: {type(keys)}")
         for i in range(len(keys)):
-            print(f"{i}: len: {len(keys[i])} : 0 shape {keys[i][0].shape}")
+            print(f"{i}: len: {len(keys[i])} : 0 shape {keys[i][1].shape}")
 
         return attention
 
