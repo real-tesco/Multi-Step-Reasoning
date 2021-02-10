3c574b9fe471d8194d3faad1e0b58627c07df1a3
real-tesco <lucaforprez@web.de>
2021-02-10 11:20:41 +0100

    removed comments

--- msr/retriever/bm25_model.py
+++ msr/retriever/bm25_model.py
@@ -4,8 +4,6 @@ from pyserini.search import SimpleSearcher
 class BM25Retriever:
     def __init__(self, dataset):
         self._searcher = SimpleSearcher(dataset)
-        # self._searcher.set_bm25(3.44, 0.87)
-        # self._searcher.set_rm3(10, 10, 0.5)
 
     def set_rm3(self, a, b, c):
         self._searcher.set_rm3(a, b, c)
